19634cc3bae21aab8efea9eaa86246e6179b8196
Zachrary Johnson <zacharytaylorjohnson1@gmail.com>
2020-06-29 17:28:13 -0600

    Finished up fashion_mnist tutorial, used 5 epochs in the model, 25 epochs seems to be where the diminishing rate of returns happens

--- tutorial_1.py
+++ tutorial_1.py
@@ -16,6 +16,24 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 train_images = train_images/255.0
 test_images = test_images/255.0
 
-plt.imshow(train_images[0], cmap=plt.cm.binary)
-plt.show()
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=5)
+
+prediction = model.predict(test_images)
+
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Actual: " + class_names[test_labels[i]])
+    plt.title("Prediction: " + class_names[np.argmax(prediction[i])])
+    plt.show()
+
+
 
